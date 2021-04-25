ac77c5eed1f018be2f4fe1cb3ad5db45607e4816
Juan Pablo Torres <jptorres1@uc.cl>
2021-04-25 00:56:49 -0400

    avances metodos get y post

---
+++ app.py
@@ -0,0 +1,235 @@
+from flask import request, Flask, Response, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from base64 import b64encode
+
+app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Artist(db.Model):
+    __tablename__ = 'artist'
+    id = db.Column(db.String(22), primary_key=True)
+    name = db.Column(db.String(200), unique=True)
+    age = db.Column(db.Integer)
+    albums= db.relationship('Album', backref='artist', lazy=True)
+    tracks = db.relationship('Track', backref='artist', lazy=True)
+    albums_url = db.Column(db.String(200), unique=True)
+    tracks_url = db.Column(db.String(200), unique=True)
+    self_url = db.Column(db.String(200), unique=True)
+
+    def __init__(self, id, name, age):
+        self.id = id
+        self.self_url = f'/artists/{id}'
+        self.name = name
+        self.age = age
+        self.albums_url = self.self_url + '/albums'
+        self.tracks_url = self.self_url + '/tracks'
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'albums': self.albums_url,
+            'tracks': self.tracks_url,
+            'self': self.self_url
+        }
+
+
+
+
+class Album(db.Model):
+    __tablename__ = 'album'
+    id = db.Column(db.String(22), primary_key=True)
+    name = db.Column(db.String(200))
+    genre = db.Column(db.String(200))
+    artist_id = db.Column(db.String(22), db.ForeignKey('artist.id'),
+        nullable=False)
+    tracks = db.relationship('Track', backref='album', lazy=True)
+    artist_url = db.Column(db.String(200))
+    tracks_url = db.Column(db.String(200))
+    self_url = db.Column(db.String(200), unique=True)
+
+
+    def __init__(self, id, name, genre, artist_id):
+        self.id = id
+        self.self_url = f'/albums/{id}'
+        self.name = name
+        self.genre = genre
+        self.artist_id = artist_id
+        self.artist_url = f'/artists/{artist_id}'
+        self.tracks_url = self.self_url + f'/tracks'
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'artist_id': self.artist_id,
+            'name': self.name,
+            'genre': self.genre,
+            'artist': self.artist_url,
+            'tracks': self.tracks_url,
+            'self': self.self_url
+        }
+
+
+
+
+class Track(db.Model):
+    __tablename__ = 'track'
+    id = db.Column(db.String(22), primary_key=True)
+    name = db.Column(db.String(200))
+    duration = db.Column(db.Float)
+    times_played = db.Column(db.Integer)
+    artist_id = db.Column(db.String(22), db.ForeignKey('artist.id'),
+        nullable=False)
+    album_id = db.Column(db.String(22), db.ForeignKey('album.id'),
+        nullable=False)
+    artist_url = db.Column(db.String(200))
+    album_url = db.Column(db.String(200))
+    self_url = db.Column(db.String(200), unique=True)
+
+    def __init__(self, id, name, duration, artist_id, album_id):
+        self.id = id
+        self.self_url = f'/tracks/{id}'
+        self.name = name
+        self.duration = duration
+        self.times_played = 0
+        self.artist_id = artist_id
+        self.album_id = album_id
+        self.artist_url = f'/artists/{artist_id}'
+        self.album_url = f'/albums/{album_id}'
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'album_id': self.album_id,
+            'name': self.name,
+            'duration': self.duration,
+            'times_played': self.times_played,
+            'artist': self.artist_url,
+            'album': self.album_url,
+            'self': self.self_url
+        }
+
+
+
+ARTIST_KEYS = ['name', 'age']
+ALBUM_KEYS = ['name', 'genre']
+TRACK_KEYS = ['name', 'duration', 'times_played']
+
+# POST
+@app.route('/artists', methods=['POST'])
+def post_artist():
+    data = dict(request.args)
+    _id = b64encode(data.get('name').encode()).decode('utf-8')[:22]
+    artist = Artist(id=_id, name=data.get('name'), age=data.get('age'))
+    db.session.add(artist)
+    db.session.commit()
+    return Response(status=201) # PENDIENTE
+
+@app.route('/artists/<string:artist_id>/albums', methods=['POST'])
+def post_album(artist_id):
+    data = dict(request.args)
+    _id = b64encode(f'{data.get("name")}:{artist_id}'.encode()).decode('utf-8')[:22]
+    album = Album(id=_id, name=data.get('name'), genre=data.get('genre'), artist_id=artist_id)
+    db.session.add(album)
+    db.session.commit()
+    return Response(status=201)
+
+@app.route('/albums/<string:album_id>/tracks', methods=['POST'])
+def post_track(album_id):
+    data = dict(request.args)
+    _id = b64encode(f'{data.get("name")}:{album_id}'.encode()).decode('utf-8')[:22]
+    artist_id = Album.query.filter_by(id=album_id).first().artist_id
+    track = Track(id=_id, name=data.get('name'), duration=data.get('duration'), 
+                    artist_id = artist_id, album_id=album_id)
+    db.session.add(track)
+    db.session.commit()
+    return Response(status=201)
+
+
+
+# GET
+@app.route('/artists', methods=['GET'])
+def artists():
+    artists_all = Artist.query.all()
+    return jsonify(artists=[a.serialize() for a in artists_all])
+   
+
+@app.route('/albums', methods=['GET'])
+def albums():
+    albums_all = Album.query.all()
+    return jsonify(albums=[a.serialize() for a in albums_all])
+
+@app.route('/tracks', methods=['GET'])
+def tracks():
+    tracks_all = Track.query.all()
+    return jsonify(tracks=[t.serialize() for t in tracks_all])
+
+
+@app.route('/artists/<string:artist_id>', methods=['GET'])
+def artist(artist_id):
+    pass
+
+@app.route('/artists/<string:artist_id>/albums', methods=['GET'])
+def artist_albums(artist_id):
+    pass
+
+@app.route('/artists/<string:artist_id>/tracks', methods=['GET'])
+def artist_tracks(artist_id):
+    pass
+
+@app.route('/albums/<string:album_id>', methods=['GET'])
+def album(album_id):
+    pass
+
+@app.route('/albums/<string:album_id>/tracks', methods=['GET'])
+def album_tracks(album_id):
+    pass
+
+@app.route('/tracks/<string:track_id>', methods=['GET'])
+def track(track_id):
+    pass
+
+
+
+# PUT
+@app.route('/artists/<string:artist_id>/albums/play', methods=['PUT'])
+def play_artists(artist_id):
+    pass
+
+@app.route('/albums/<string:album_id>/tracks/play', methods=['PUT'])
+def play_album(album_id):
+    pass
+
+@app.route('/tracks/<string:track_id>/play', methods=['PUT'])
+def play_track(track_id):
+    pass
+
+
+
+#DELETE
+@app.route('/artists/<string:artist_id>', methods=['DELETE'])
+def delete_artist(artist_id):
+    pass
+
+@app.route('/albums/<string:album_id>', methods=['DELETE'])
+def delete_album(album_id):
+    pass
+
+@app.route('/tracks/<string:track_id>', methods=['DELETE'])
+def delete_track(track_id):
+    pass
+
+if __name__ == '__main__':
+    app.run()
